41c3b418fa86cae70cf9f0c4e63c1a20f5fd321c
Armin Thomas <armin.thms@gmail.com>
2022-05-26 16:00:58 +0200

    Deleted via the Open Science Framework

--- scripts/fmriprep/build_fmriprep_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# make sure dir for image exists
-mkdir -p "$STUDY_DIR/images/"
-
-# build image
-singularity build "$STUDY_DIR/images/fmriprep-20.2.3.simg" docker://nipreps/fmriprep:20.2.3
--- scripts/fmriprep/download_dataset_on.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-aws s3 sync --no-sign-request "s3://openneuro.org/$DATASET" "$STUDY_DIR/data"
